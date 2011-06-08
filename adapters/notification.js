6f7187ea0d10a48e0e6375753e18f2607f2aa58d
Marielle Lange <mlange@widged.com>
2011-06-08 16:49:45 +1200

    Simplistic notification adapter

--- adapters/notification.js
+++ adapters/notification.js
@@ -8,11 +8,50 @@
  http://github.com/widged/widgeds
 */
 
-;(function(){
-    var plugin = {};
+;(function($){
+    var plugin = {
+       run: function(selector, typeId, settings) {
+          $(selector).bind("notify", plugin.showMessage);
+          plugin.settings = $.extend(plugin.defaults, settings);
+       },
+       defaults: {
+          css:{left: "0px", right: "0px", "min-height":"40px","border":"3px solid orange","margin":"6px 0px","text-align":"center"},
+          message: '<span style="font-size: 0.8em">(Click To hide)</span><br/>',
+          hideEvent:"click"
+       },
+       showMessage: function(e, data) {
+         console.log("[showMessage]", e, data); 
+         var sel = "#" + e.target.id + "-notification";
+         var $box = $(sel);
+         var $el = $("<div/>")
+         $box.prepend($el);
+         $el.hide();
+         $el.append(plugin.settings["message"])
+         $el.append(data.msg);
+         $el.css(plugin.settings["css"]);
+
+         switch(data.type)
+         {
+            case 1:
+               $el.css({"background-color":"#EAF7DA","border-color":"#79BA25"}); // green
+               break;
+            case 2:
+               $el.css({"background-color":"#FFD9DA","border-color":"#F05258"}); // red
+               break;
+            default:
+               $el.css({"background-color":"#FFFFFF","border-color":"#4C4C4C"}); // gray
+               break;
+         }
+         $el.bind(plugin.settings["hideEvent"],function(){
+            $el.slideToggle();
+         });
+         $el.slideToggle();
+            
+       }
+    };
 
 
     if(!window.widged){window.widged={};}//We create a shortcut for our framework, we can call the methods by $$.method();
    $.extend(true, window.widged, {notification: plugin});
-})();
+})(jQuery);
 
